a572a89804bbfb63b54c47435367d16f0aaf4441
Klimov Eduard <edvlklimov@gmail.com>
2024-06-14 17:21:29 +0300

    task is completed

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/RoleDaoImp.java
@@ -0,0 +1,22 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+import org.springframework.stereotype.Repository;
+import ru.kata.spring.boot_security.demo.models.Role;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class RoleDaoImp implements RoleDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Role> listRoles() {
+        TypedQuery<Role> query = entityManager.createQuery("from Role", Role.class);
+        return query.getResultList();
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/RoleServiceImp.java
@@ -0,0 +1,26 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.RoleDao;
+import ru.kata.spring.boot_security.demo.models.Role;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImp implements RoleService{
+
+    private final RoleDao roleDao;
+
+    @Autowired
+    public RoleServiceImp(RoleDao roleDao) {
+        this.roleDao = roleDao;
+    }
+
+    @Transactional
+    @Override
+    public List<Role> listRoles() {
+        return roleDao.listRoles();
+    }
+}
